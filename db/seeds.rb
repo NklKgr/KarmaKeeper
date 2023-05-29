47a70cf384d46955200c72bd18cc9e5edb729a97
kantonio88 <keston.antonio.schmidtgen@gmail.com>
2023-05-29 18:01:14 +0200

    Created the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(email: "test@bla.com", password: "123456")
+Product.create!(name: 'Private Jets', overview: 'Unlimited carbon footprint', unit: 'hours', price: 1000.00)
+Product.create!(name: 'Luxuary Cars', overview: 'Unlimited carbon emissions', unit: 'hours', price: 2000.00)
+Product.create!(name: 'Private Yachts', overview: 'Unlimited marine pollution', unit: 'hours', price: 3000.00)
+Product.create!(name: 'Luxury vacations', overview: 'Unlimited greenhouse emissions & environmental damage ', unit: 'hours', price: 5000.00)
