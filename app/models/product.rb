1f0edeb4a849c0540a0be976bf334013c419613b
Gilad Milashut <gilad.milashut@gmail.com>
2023-05-29 16:16:06 +0200

    created validations and relations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
