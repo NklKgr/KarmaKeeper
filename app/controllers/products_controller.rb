3eead61d80f778acf2adab7928bc8e3f67f1d33f
kantonio88 <keston.antonio.schmidtgen@gmail.com>
2023-05-30 18:01:11 +0200

    create action and form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,18 +11,51 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
-  private
 
-  def set_products
-    @product = Product.find(params[:id])
-  end
-
-  def create
+  def create<div class="product-form">
+    <%= simple_form_for @product, url: products_path, method: :post do |f| %>
+      <div class="form-group">
+        <%= f.label :name %>
+        <%= f.text_field :name, class: 'form-control' %>
+      </div>
+      <div class="form-group">
+        <%= f.label :overview %>
+        <%= f.text_area :overview, class: 'form-co<div class="product-form">
+        <%= simple_form_for @product, url: products_path, method: :post do |f| %>
+          <div class="form-group">
+            <%= f.label :name %>
+            <%= f.text_field :name, class: 'form-control' %>
+          </div>
+          <div class="form-group">
+            <%= f.label :overview %>
+            <%= f.text_area :overview, class: 'form-control' %>
+          </div>
+          <div class="form-group">
+            <%= f.label :price %>
+            <%= f.text_field :price, class: 'form-control' %>
+          </div>
+          <div class="form-group">
+            <%= f.label :unit %>
+            <%= f.text_field :unit, class: 'form-control' %>
+          </div>
+          <%= f.submit "Create Product", class: "btn btn-primary" %>
+        <% end %>
+      </div>ntrol' %>
+      </div>
+      <div class="form-group">
+        <%= f.label :price %>
+        <%= f.text_field :price, class: 'form-control' %>
+      </div>
+      <div class="form-group">
+        <%= f.label :unit %>
+        <%= f.text_field :unit, class: 'form-control' %>
+      </div>
+      <%= f.submit "Create Product", class: "btn btn-primary" %>
+    <% end %>
+  </div>
     @product = Product.new(product_params)
     @product.user = current_user
     if @product.save
-      raise
-      puts ActiveRecord::Base.connection.to_sql # add this line to log SQL statement
       redirect_to products_path, notice: "Product was successfully created."
     else
       puts @product.errors.full_messages # add this line to log errors
@@ -33,6 +66,10 @@ class ProductsController < ApplicationController
   private
 
   # Strong params: white list of sanitized input
+  def set_products
+    @product = Product.find(params[:id])
+  end
+
   def product_params
     params.require(:product).permit(:name, :overview, :price, :unit)
   end
