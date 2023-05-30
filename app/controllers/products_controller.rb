5247b848f40170c40dda66041c3ac387d35d15ea
Gilad Milashut <gilad.milashut@gmail.com>
2023-05-30 12:33:20 +0200

    created the action for show as also strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,20 @@
 class ProductsController < ApplicationController
+  before_action :set_products, only: [:show]
+
   def index
     @products = Product.all
+  end
+
+  def show;  end
+
+  private
+
+  def set_products
+    @product = Product.find_by(id: params[:id])
+  end
+
+  # Strong params: white list of sanitized input
+  def product_params
+    params.require(:product).permit(:name, :overview, :price, :unit)
   end
 end
