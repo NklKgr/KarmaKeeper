b9f81dceaf8cdb8f2d669969304e3c24b672d5ce
Nkl Kgr <krueger.lup@web.de>
2023-05-30 17:30:32 +0200

    save work

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,27 @@
 class BookingsController < ApplicationController
-# action for a new booking
-  
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:duration, :people_quantity, :product_id, :user_id)
+  end
 end
+
+
+# to be pasted in the view#index
+# <div data-controller="toggle" class="mt-5">
+#   <button class="btn btn-outline-primary">Add a new monument</button>
+#   <%= render "form", monument: @monument %>
+# </div>
