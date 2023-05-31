70656fdc99a1c4485b8b518e014e5a3b5e560c9d
Gilad Milashut <gilad.milashut@gmail.com>
2023-05-31 08:56:59 +0200

    reaadding form for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,21 @@ class ProductsController < ApplicationController
 
   def show; end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    if @product.save
+      redirect_to products_path, notice: "Product was successfully created."
+    else
+      puts @product.errors.full_messages # add this line to log errors
+      render :new
+    end
+  end
+
   private
 
   def set_products
