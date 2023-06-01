263272d766a559b24d0d7df7fdf3ed7f293e797b
Carolina Rusche <carolinarusche@gmail.com>
2023-06-01 11:59:07 +0200

    Searchbar functionality done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @product = Product.new
+
+    query = params[:query]
+    if query.present?
+      sql_query = "name ILIKE :query OR overview ILIKE :query"
+      @products = @products.where(sql_query, query: "%#{params[:query]}%")
+    end
+
+    # if params[:year].present?
+    #   @products = @products.where(year: params[:year])
+    # end
   end
 
   def show
@@ -23,15 +33,6 @@ class ProductsController < ApplicationController
     else
       puts @product.errors.full_messages
       render :new
-    end
-  end
-
-  def search
-    if params[:search].blank?
-      redirect_to products_path and return
-    else
-      @parameter = params[:search].downcase
-      @results = Products.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
     end
   end
 
