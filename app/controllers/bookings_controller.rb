406d350285ba853ed991c2241c7121c74cb9690d
Gilad Milashut <gilad.milashut@gmail.com>
2023-06-02 11:37:52 +0200

    added a delete button

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,6 +19,12 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
   private
 
   def booking_params
